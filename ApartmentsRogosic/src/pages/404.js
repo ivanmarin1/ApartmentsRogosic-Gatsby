56ca9a886ef07a84880df374e112053e2388846a
Ivan Marin <ivan.marin2525@gmail.com>
2020-02-18 18:47:00 +0100

    Modify to center text

--- ApartmentsRogosic/src/pages/404.js
+++ ApartmentsRogosic/src/pages/404.js
@@ -1,13 +1,15 @@
 import React from "react"
-
+import styles from "../styles/layout.module.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.mainContainer}>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
