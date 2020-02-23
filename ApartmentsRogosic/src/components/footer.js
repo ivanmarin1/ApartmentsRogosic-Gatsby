4009cb9b873e2de8b0c61f0d07216b37d4c2a0c5
Ivan Marin <ivan.marin2525@gmail.com>
2020-02-23 14:07:16 +0100

    Add translation for text

--- ApartmentsRogosic/src/components/footer.js
+++ ApartmentsRogosic/src/components/footer.js
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "../styles/footer.module.css"
+import { useTranslation } from "react-i18next"
 
 const Footer = () => {
+  const { t } = useTranslation()
   return (
     <footer>
-      <p className={styles.footerText}>Apartments Rogošić</p>
+      <p className={styles.footerText}>{t("site.footer")}</p>
     </footer>
   )
 }
