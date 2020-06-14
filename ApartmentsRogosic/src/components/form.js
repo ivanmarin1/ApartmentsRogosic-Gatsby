782afb53794bb0f1eb4e84f570ec507ffa5d979c
Ivan Marin <ivan.marin2525@gmail.com>
2020-06-14 16:47:30 +0200

    Try to add form success page

--- ApartmentsRogosic/src/components/form.js
+++ ApartmentsRogosic/src/components/form.js
@@ -5,6 +5,7 @@ import * as Yup from "yup"
 import classnames from "classnames"
 import { useTranslation } from "react-i18next"
 import WithTranslateFormErrors from "../i18n/useTranslateFormErrors"
+import PopUp from "./popUp"
 
 const InputFeedback = ({ error }) =>
   error ? <div className="input-feedback">{error}</div> : null
@@ -158,7 +159,7 @@ const App = ({ apartment }) => {
             body: encode({ "form-name": "booking", ...values }),
           })
             .then(() => {
-              alert("Success")
+              return <PopUp />
             })
             .catch(() => {
               alert("Error")
