01d7916fbe698751c84d8c2b483bbe216deb0511
Ivan Marin <ivan.marin2525@gmail.com>
2020-10-26 18:52:27 +0100

    Fix bug form 3

--- ApartmentsRogosic/src/components/form.js
+++ ApartmentsRogosic/src/components/form.js
@@ -349,7 +349,10 @@ const App = ({ apartment }) => {
                     label={t("apartments.apartment1")}
                     aria-label={t("apartments.apartment1")}
                     onClick={() =>
-                      setFieldValue("apartmentNum", t("apartments.apartment1"))
+                      setFieldValue(
+                        values.apartmentNum,
+                        t("apartments.apartment1")
+                      )
                     }
                   />
                   <option
